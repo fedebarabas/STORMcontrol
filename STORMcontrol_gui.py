e0f1aa8ca4e865bbfddfcdc983b19f9012209692
Federico Barabas <fede.barabas@gmail.com>
2013-07-29 18:45:24 -0300

    Add U12 port as an argument

--- STORMcontrol_gui.py
+++ STORMcontrol_gui.py
@@ -7,8 +7,6 @@ import lantz.ui
 from lantz import Q_
 from lantz.ui.qtwidgets import connect_driver
 from Qt.QtGui import QApplication, QWidget
-#from PySide.QtGui import *
-#from PySide.QtCore import *
 from Qt.uic import loadUi
 from Qt.QtCore import QThread, QObject
 
@@ -29,12 +27,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p640', '--port640', type=str, default='COM3', help='COM port for the 640nm laser')
 parser.add_argument('-p405', '--port405', type=str, default='COM4', help='COM port for the 405nm laser')
 parser.add_argument('-pz', '--portz', type=str, default='COM1', help='COM port for the Z stage or the ProScanII controller if the NanoScanZ is connected to it through its RS232-2 port')
+parser.add_argument('-pu12', '--portu12', type=int, default=0, help='Port for the LabJack U12 interface')
 args = parser.parse_args()
 
 with MiniLasEvo(args.port640) as laser640, \
      MiniLasEvo(args.port405) as laser405, \
      NanoScanZ_chained(args.portz) as stagez, \
-     U12(0) as u12:
+     U12(args.portu12) as u12:
 
     ### CHECKING THE ACCESS TO ALL INSTRUMENTS
     print(laser640.idn)
