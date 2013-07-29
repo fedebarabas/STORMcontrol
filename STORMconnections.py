d7fbb0ee58e99a11a59493c888a8a82daab93e4a
Federico Barabas <fede.barabas@gmail.com>
2013-07-29 19:41:47 -0300

    focus_lock out of the if not None

--- STORMconnections.py
+++ STORMconnections.py
@@ -53,10 +53,10 @@ class Connections(Form_class):
             connect_driver(main, laser640, prefix='laser640')
             connect_driver(main, laser405, prefix='laser405')
             connect_driver(main, stagez, prefix='stagez')
-            focus_lock_on = main.findChild((QWidget, ), 'focus_lock_on')
         
             ### MOVE Z STAGE TO ITS DYNAMIC RANGE CENTER AND DEFINE IT AS Z = 0
             stagez.move_absolute(50 * um)
             stagez.position = 0 * um
     
-            focus_lock_on.clicked.connect(handletoggle)
+        focus_lock_on = self.findChild((QWidget, ), 'focus_lock_on')
+        focus_lock_on.clicked.connect(handletoggle)
