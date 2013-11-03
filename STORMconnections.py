842c744750477ee1c93e86f2c968d7754f6ea74f
Federico Barabas <fede.barabas@gmail.com>
2013-11-03 23:14:38 +0100

    PyQtgraph embedding

--- STORMconnections.py
+++ STORMconnections.py
@@ -3,64 +3,74 @@
 from __future__ import with_statement
 
 import time
-
-#from numpy import arange, sin, pi
 import numpy as np
+import math
+import random
+import collections
 
-# QT IMPORTS
 from PyQt4.uic import loadUi
-from PyQt4.QtCore import QThread, QObject, QTimer, SIGNAL, SLOT
-from PyQt4.QtGui import QWidget, QMainWindow, qApp, QApplication
-
+from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 
 from lantz.ui.qtwidgets import connect_driver
 from lantz import Q_
+from pint import UnitRegistry
 
+ureg = UnitRegistry()
 um = Q_(1, 'micrometer')
 
-class DataGen(object):
-    """ A silly class that generates pseudo-random data for
-        display in the plot.
-    """
-    def __init__(self, init=50):
-        self.data = self.init = init
+def amplitude():
+    frequency = 0.5
+    noise = random.normalvariate(0., 1.)
+    new = 10.*math.sin(time.time()*frequency*2*math.pi) + noise
+    return new * ureg.volt
 
-    def next(self):
-        self._recalc_data()
-        return self.data
+class DynamicPlotter():
 
-    def _recalc_data(self):
-        delta = np.random.uniform(-0.5, 0.5)
-        r = np.random.random()
+    def __init__(self, widget, func, sampleinterval=0.1, timewindow=10.):
+        # Data stuff
+        self._interval = int(sampleinterval*1000)
+        self._bufsize = int(timewindow/sampleinterval)
+        self.databuffer = collections.deque([0.0]*self._bufsize, self._bufsize)
+        self.x = np.linspace(-timewindow, 0.0, self._bufsize)
+        self.y = np.zeros(self._bufsize, dtype=np.float)
+        
+        self.func = func
+      
+        self.plt = widget
+        
+        pg.setConfigOptions(antialias=True)
+#        self.plt = pg.plot(title='Dynamic Plotting with PyQtGraph')
+        size = [widget.width(), widget.height()]
+        self.plt.resize(*size)
+        self.plt.showGrid(x=True, y=True)
+        self.plt.setLabel('left', self.func.__name__, self.func().units)
+        self.plt.setLabel('bottom', 'time', 's')
+        self.curve = self.plt.plot(self.x, self.y, pen='y')
+        # QTimer
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateplot)
+        self.timer.start(self._interval)
+        
+    def updateplot(self):
+        self.databuffer.append( self.func().magnitude )
+        self.y[:] = self.databuffer
+        self.curve.setData(self.x, self.y)
 
-        if r > 0.9:
-            self.data += delta * 15
-        elif r > 0.8:
-            # attraction to the initial value
-            delta += (0.5 if self.init > self.data else -0.5)
-            self.data += delta
-        else:
-            self.data += delta
-
-class Connections(QMainWindow):
+class Connections(QtGui.QMainWindow):
 
     def __init__(self, laser640, laser405, zstage, daq, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
 
         self.ui = loadUi('STORMgui.ui', self)
-        self.ui.show()
-
-        QObject.connect(self.actionQuit, SIGNAL('triggered()'), qApp,
-        SLOT("quit()"))
-
-        #QObject.connect(self.pushButton, SIGNAL("clicked()"), self.update_graph)
+        self.ui.actionQuit.triggered.connect(QtGui.qApp.quit)
+       
 
         ### FOCUS LOCK FUNCTION
-        class Focus_lock(QThread):
+        class Focus_lock(QtCore.QThread):
             def __init__(self, parent=None):
-                QThread.__init__(self, parent)
+                QtCore.QThread.__init__(self, parent)
                 self.exiting = False
 
             def run(self):
@@ -83,6 +93,8 @@ class Connections(QMainWindow):
                     time.sleep(0.01)
                     continue
 
+        self.ui.focus_lock_on.clicked.connect(handletoggle)
+
         instruments = [laser640, laser405, zstage, daq]
 
         if not None in instruments:
@@ -90,10 +102,10 @@ class Connections(QMainWindow):
             ### MAXIMUM POWER BUTTONS
             def set_powermax(inst):
                 inst.power = inst.maximum_power
-            laser640__powermax = self.findChild((QWidget, ),
+            laser640__powermax = self.findChild((QtGui.QWidget, ),
             'laser640__powermax')
             laser640__powermax.clicked.connect(lambda: set_powermax(laser640))
-            laser405__powermax = self.findChild((QWidget, ),
+            laser405__powermax = self.findChild((QtGui.QWidget, ),
             'laser405__powermax')
             laser405__powermax.clicked.connect(lambda: set_powermax(laser405))
 
@@ -105,28 +117,4 @@ class Connections(QMainWindow):
             ### MOVE Z STAGE TO ITS DYNAMIC RANGE CENTER AND DEFINE IT AS Z = 0
             zstage.move_absolute(50 * um)
             zstage.position = 0 * um
-
-        focus_lock_on = self.findChild((QWidget, ), 'focus_lock_on')
-        focus_lock_on.clicked.connect(handletoggle)
-
-        #timer = QTimer(self)
-        #QObject.connect(timer, SIGNAL("timeout()"), self.ui.mpl.canvas.update_figure)
-        #timer.start(1000)
-
-        #pg.lockplot.setLabel('left', 'Value', units='V')
-        #pg.lockplot.setLabel('bottom', 'Time', units='s')
-        lockplot = pg.PlotWidget()
-        pg.setConfigOptions(antialias=True)
-        curve = lplot.plot(pen='y')
-        data = np.random.normal(size=(10,1000))
-        ptr = 0
-        def update():
-            global curve, data, ptr, p6
-            curve.setData(data[ptr%10])
-            if ptr == 0:
-                lplot.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
-            ptr += 1
-        timer = QTimer()
-        timer.timeout.connect(update)
-        timer.start(50)
         
